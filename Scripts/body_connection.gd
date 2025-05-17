024c90c60d7235021c253fb535c87ef64da87c06
Nyx <nj.wissema@gmail.com>
2025-05-17 14:25:47 +0200

    update

--- Scripts/body_connection.gd
+++ Scripts/body_connection.gd
@@ -7,20 +7,35 @@ signal connection_achieved(Line:BodyConnection, CurrentLength: float, DesiredLen
 
 @export var body1: PlayerBody
 @export var body2: PlayerBody
+
+@export var debug_printing = false
+@export var debug_length_display = false
+@export var debug_percentage_display = false
+
+var connected: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.add_point(body1.global_position)
 	self.add_point(body2.global_position)
+	#default_color = get_colour(body1.global_position, body2.global_position)
 	pass # Replace with function body.
 
-func get_length(pos1: Vector2, pos2: Vector2)-> float : 
-	return 0.0
 
+func get_percentage(pos1: Vector2, pos2: Vector2) -> float:
+	var length = pos1.distance_to(pos2)
+	return (length - desired_length) / (max_length - desired_length)
+
+func get_colour(percentage: float) -> Color:
+	return Color(percentage, (1 - percentage), 0, 1 if percentage <= 1 else 0)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.set_point_position(0, body1.global_position)
 	self.set_point_position(1, body2.global_position)
 	
+	var percentage = get_percentage(body1.global_position, body2.global_position)
+	connected = (true if percentage <= 1 else false)
 	
-	
-	#default_color = Color()
+	self.default_color = get_colour(percentage)
+	if debug_printing:
+		print(self.default_color)
